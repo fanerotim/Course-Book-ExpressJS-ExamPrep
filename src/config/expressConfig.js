412ba3eb3ee2f7c59ad5993291f02646e478049f
Petar Cholakov <petar.cholakov30@gmail.com>
2024-02-06 16:56:42 +0000

    rendered register page

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path')
 
 const expressConfig = (app) => {
-    app.use(express.static(path.resolve('src/static')));
+    app.use(express.static('src/static'));
     return app;
 }
 
