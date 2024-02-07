b7d4f45c1fd366bf09d2db27704ee810eb00c2fb
Petar Cholakov <petar.cholakov30@gmail.com>
2024-02-07 15:14:07 +0000

    set up home page to dynamically load only the last three courses

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const Course = require('../models/Course');
 
-router.get('/', (req, res) => {
-    res.render('home')
+router.get('/', async (req, res) => {
+    let lastThree = await Course.find().sort({_id: -1}).limit(3).lean();
+
+    res.render('home', {lastThree});
 })
 
 router.get('/catalog', async (req, res) => {
