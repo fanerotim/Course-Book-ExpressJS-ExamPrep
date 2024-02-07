528ea6ba3e28989eb6b3f11f30b0b2b2f33a9427
Petar Cholakov <petar.cholakov30@gmail.com>
2024-02-07 09:53:14 +0000

    set up catalog page to load courses dynamically

--- src/controllers/createController.js
+++ src/controllers/createController.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const createService = require('../services/createService');
 const jwt = require('jsonwebtoken');
 
+const SECRET = require('../config/config')
+
 router.get('/create', (req, res) => {
     res.render('create');
 })
@@ -10,12 +12,13 @@ router.post('/create', async (req, res) => {
     let userData = req.body;
     
     let token = req.cookies['auth'];
-    let loggedUser = jwt.decode(token, 'adshjapdsipo234mnlfs-09i23-05rlesw');
+    let loggedUser = jwt.decode(token, SECRET);
 
     userData['owner'] = loggedUser._id;
-    
 
     await createService.create(userData);
+
+    res.redirect('/catalog')
 })
 
 module.exports = router;
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const Course = require('../models/Course');
 
 router.get('/', (req, res) => {
     res.render('home')
+})
+
+router.get('/catalog', async (req, res) => {
+
+    let courses = await Course.find().lean();
+
+    res.render('catalog', {courses});
 })
 
 module.exports = router;
