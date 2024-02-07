cfb2f3211d2a9709d38a1ed0d1f3412ea22588ae
Petar Cholakov <petar.cholakov30@gmail.com>
2024-02-07 08:20:51 +0000

    rendered create course page

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const homeController = require('../controllers/homeController');
 const authController = require('../controllers/authController');
+const createController = require('../controllers/createController');
 
 router.use(homeController);
 router.use(authController);
+router.use(createController);
 
 module.exports = router;
